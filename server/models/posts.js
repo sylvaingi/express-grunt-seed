5116bb0eb14f21c6cac6319a1704aafedea71fe8
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-04-04 20:50:49 +0200

    Set mongo option as it was annoying about it

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mongo = require('mongoskin');
-var db = mongo.db('localhost:27017/express-seed');
+var db = mongo.db('localhost:27017/express-seed', {safe: true});
 
 var posts = db.collection('posts');
 
